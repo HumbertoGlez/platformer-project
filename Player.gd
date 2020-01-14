d95167b8b5a6263dc8a3b1b0108161f049d7950f
Humberto Gonzalez <hglez.98s@gmail.com>
2020-01-14 02:03:26 -0600

    jump and animations

--- Player.gd
+++ Player.gd
@@ -7,19 +7,31 @@ const FLOOR = Vector2(0, -1);
 
 var velocity = Vector2();
 
+onready var animator = get_node("AnimationPlayer");
+onready var sprite = get_node("Sprite");
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED;
+		sprite.set_flip_h(false);
+		if animator.get_current_animation() != "Run" and is_on_floor():
+			animator.play("Run");
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED;
+		sprite.set_flip_h(true);
+		if animator.get_current_animation() != "Run" and is_on_floor():
+			animator.play("Run");
 	else:
 		velocity.x = 0;
+		if animator.get_current_animation() != "Idle" and is_on_floor():
+			animator.play("Idle");
 	
 	if Input.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_POWER;
+		if animator.get_current_animation() != "Jump":
+			animator.play("Jump");
 	
 	velocity.y += GRAVITY;
 	
